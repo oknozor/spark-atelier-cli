3f7c400ea3f742e26a7a37f582b75725335c4245
Paul Delafosse <paul.delafosse@protonmail.com>
2019-11-20 13:51:47 +0100

    refactor(*): remove git2

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,35 @@
-use git2::IndexAddOption;
-use git2::Repository;
-use std::path::Path;
 use std::process::Command;
 
-fn main() -> Result<(), git2::Error> {
-
-    let mvn_test  = Command::new("mvn").arg("test").output();
-    let mvn_test  = mvn_test.expect("command `mvn test` not found");
+fn main() -> Result<(), std::io::Error> {
+    let mvn_test = Command::new("mvn").arg("test").output();
+    let mvn_test = mvn_test.expect("command `mvn test` not found");
     println!("{}", String::from_utf8(mvn_test.stdout).unwrap());
 
-    // Get repository on the current path
-    let repo = match Repository::open(&Path::new(".")) {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
-
-    // This callback is called on each out of index file
-    let git_add_callback = &mut |path: &Path, _matched_spec: &[u8]| -> i32 {
-        let status = repo.status_file(path).expect("unable to get `git status`");
-
-        if status.contains(git2::Status::WT_MODIFIED) || status.contains(git2::Status::WT_NEW) {
-            0 // Flag new or modified files for add
-        } else {
-            1 // deny other
-        }
-    };
-
-    // get the current repo index
-    let mut index = repo.index()?;
-
-    // get the current repo index
     if mvn_test.status.success() {
-        println!("ok");
-        index
-            .add_all(vec!["."], IndexAddOption::DEFAULT, Some(git_add_callback))
-            .unwrap();
+
+        let git_add = Command::new("git").arg("add").arg("-A").output();
+        let git_add = git_add.expect("command `git add -A` failed");
+        println!("{}", String::from_utf8(git_add.stdout).unwrap());
+        
+        let git_commit = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg("\"solve step\"")
+        .output();
+
+        let _ = git_commit.expect("command `git commit` failed");
+        
+        let git_merge = Command::new("git")
+        .arg("merge")
+        .arg("--no-ff")
+        .arg("step2")
+        .output();
+
+        let git_merge = git_merge.expect("command `git merge` failed");
+        println!("{}", String::from_utf8(git_merge.stdout).unwrap());
     } else {
-        eprintln!("No changes in the current index")
+        eprint!("Test failedtry again ! ");
     }
 
-    // flush changes
-    index.write()?;
     Ok(())
 }
--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 cargo build
 mv target/debug/next spark-atelier-cli-test/
 cd spark-atelier-cli-test
+touch pouet
 ./next
